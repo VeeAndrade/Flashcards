db9cc7d4c4e6c2228f282fd689f391507fe844b1
Veronica Andrade <veronica726@gmail.com>
2019-11-19 17:15:48 -0700

    Write out round class and pass all test

---
+++ src/Round.js
@@ -0,0 +1,34 @@
+const Turn = require('../src/Turn')
+
+class Round {
+  constructor(deck) {
+    this.deck = deck.cards
+    this.turns = 0
+    this.incorrectGuesses = [];
+  }
+
+  returnCurrentCard() {
+    return this.deck[0];
+  }
+
+  takeTurn(guess) {
+    let currentCard = this.returnCurrentCard()
+    let turn = new Turn(guess, currentCard)
+    this.turns += 1
+    this.deck.shift();
+    if(!turn.evaluateGuess(guess)){
+      this.incorrectGuesses.push(currentCard.id)
+    }
+    return turn.giveFeedback();
+  }
+
+  calculatePercentCorrect() {
+    return parseInt(((this.turns - this.incorrectGuesses.length)/this.turns) * 100)
+  }
+
+  endRound() {
+    return `** Round over! ** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`
+  }
+}
+
+module.exports = Round;
