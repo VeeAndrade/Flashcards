13ee3cd0bb74d77887bac95325511b19acbafe95
Veronica Andrade <veronica726@gmail.com>
2019-11-19 18:31:37 -0700

    Bulid out Game class and pass test

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,24 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
-
+const Deck = require('../src/Deck.js');
+const Card = require('../src/Card');
+const Round = require('../src/Round');
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+  }
+
+  start() {
+    let deckOfCards = prototypeQuestions.map(card => {
+      return new Card(card.id, card.question, card.answers, card.correctAnswer)
+    })
+    let deck = new Deck(deckOfCards)
+    let round = new Round(deck)
+    this.currentRound = round
+    this.printMessage(deck, round)
+    this.printQuestion(round);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
